a806a933532c0866ebe2d5a9c0b5990b10c8ecce
MelonMan <73000042+MelonicOverlord@users.noreply.github.com>
2020-12-23 15:02:10 +0000

    Updated BotData.js - Improved Some Bot Answers
    Improved some bot answers to be easier to read

--- BotData.js
+++ BotData.js
@@ -12,16 +12,16 @@ var DevPromt = [
 ];
 
 var Errors = [
-    Error6 = "**:warning: Error 6:** ```Invalid description or argument detected! Try making sure the command is being performed right and try again!```",
+    Error6 = "**:warning: Error 6:** ```Invalid description or argument detected! Try making sure the command is being performed correctly and try again!```",
     Error4 = "**:warning: Error 4:** ```This command/feature has not yet been implemented yet! Please check back in the next bot update! Thank you!```",
     Error2 = "**:shield: Error 2:** Outdated bot version! Please update to the new Bot Version for the latest features, commands and security fixes!",
     Error3 = "**:warning: Error 3:** ```Sorry, this command/feature hasn't been implemented yet! Check back in future updates!```",
-    Error5 = "**:no_entry_sign: Error 5:** You do NOT have the permission to perform this command/feature! :no_entry_sign:",
+    Error5 = "**:no_entry_sign: Error 5:** You do not have the permission to perform this command/feature! :no_entry_sign:",
     Error7 = "**:warning: Error 7:** ```There was no description given! Please give a description or argument!```",
-    StaffUser = ":no_entry_sign: Sorry, you can't perform this command on a staff member!",
-    PermissionError = ":no_entry_sign: You don't have the  correct permission!",
+    StaffUser = ":no_entry_sign: Sorry, you cannot perform this command on a staff member!",
+    PermissionError = ":no_entry_sign: You don't have the correct permission!",
     DMMessage = ":warning: You can't use this command in DM's!",
-    NullUser = ":warning: Sorry, I couldn't find that user",
+    NullUser = ":warning: Sorry, I couldn't find that user!",
     Error1 = "**:warning: Error 1:** You have no xp!",
 ];
 
